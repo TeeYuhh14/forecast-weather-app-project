24f052801c8421697300634d843b400b7040c6ad
TeeYuhh14 <tiademas@gmail.com>
2024-03-08 22:04:42 +0200

    Add axios

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ function handleSearchSubmit(event) {
   console.log(searchInput.value);
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+
+  //let apiKey = "48oeda5act0b33557f4765dc458f7f7f";
+  // let apiUrl =
+  //   "https://api.shecodes.io/weather/v1/current?query={query}&key={key}";
 }
 
 let searchFormElement = document.querySelector("#search-form");
